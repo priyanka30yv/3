2b43bebf0cb23ec65835047a93f00127e16316de
Priyanka Yadav <155914158+priyanka30yv@users.noreply.github.com>
2024-05-10 18:33:00 +0530

    Add files via upload
    class library

---
+++ Class1.vb
@@ -0,0 +1,11 @@
+﻿Public Class MusicalIntrument
+    Public instrumentName As String
+    Public price As Integer
+    Public Function RevisedPrice(discount As Double) As Double
+        Dim rp As Double
+        rp = price - (price * discount)
+        Return rp
+    End Function
+
+
+End Class
